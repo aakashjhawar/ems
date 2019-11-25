f417a379c47e691ce20a7be127198437cf1eca97
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-11-26 00:23:34 +0530

    Form control value changes

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,20 +8,28 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
-
+  fullNameLength = 0;
+  
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
-      fullName: [''],
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
       email: [''],
       skills: this.fb.group({
         skillName: [''],
         experienceInYears: [''],
         proficiency: ['beginner']
       })
+    });
 
-    })
+    this.employeeForm.valueChanges.subscribe( value => {
+      console.log(JSON.stringify(value));
+    });
+    
+    this.employeeForm.get('fullName').valueChanges.subscribe((value: string) => {
+      this.fullNameLength = value.length;
+    });
   }
 
   onLoadDataClick() :void {
